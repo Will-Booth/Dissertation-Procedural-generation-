5f0d583728cfb71b35fc5dfedff103d389935518
william booth <billaus.booth@outlook.com>
2019-07-04 15:42:08 +0100

    Add files via upload

---
+++ Fractal Generation/FractalGenerator.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FractalGenerator : MonoBehaviour {
+
+    public string seed;
+    [Range(0.1f, 1.0f)]
+    public float roughness = 0.5f;
+
+    System.Random RandomValues;
+    float[,] heightmap;
+    TerrainData td;
+    private int hash;
+    public int size;
+    int max;
+
+    void Awake()
+    {
+
+        td = GetComponent<Terrain>().terrainData;
+        size = td.heightmapResolution;
+        max = size - 1;
+    }
+
+    //garthering the x and y values of the heightmap
+    void Start()
+    {
+        heightmap = new float[size, size];
+        for (int x = 0; x < size; x++)
+        {
+            for (int y = 0; y < size; y++)
+            {
+                heightmap[x, y] = 0.5f;
+            }
+        }
+        //calling GenerateTerrain
+        GenerateTerrain();
+    }
+
+    //generating the terrain and storing it in the hash key
+    void GenerateTerrain()
+    {
+        if (seed != "")
+        {
+            hash = seed.GetHashCode();
+            RandomValues = new System.Random(hash);
+        }
+        else
+        {
+            RandomValues = new System.Random();
+        }
+        FractalCreation(max);
+        td.SetHeights(0, 0, heightmap);
+    }
+
+    //FractalCreation get the two methods diamonds and squares and applies them to the fractal equation to create a realistic map
+    void FractalCreation(int size)
+    {
+        int half = size / 2;
+        if (half < 1)
+        {
+            return;
+        }
+        for (int y = half; y < max; y += size)
+        {
+            for (int x = half; x < max; x += size)
+            {
+                //making sure the floating point numbers of the x and y values are random everytime they're generated
+                Square(x, y, half, ((float)RandomValues.NextDouble() - 0.5f) / (max / size));
+            }
+        }
+        for (int y = 0; y <= max; y += half)
+        {
+            for (int x = (y + half) % size; x <= max; x += size)
+            {
+                //making sure the floating point numbers of the x and y values are random everytime they're generated
+                Diamond(x, y, half, ((float)RandomValues.NextDouble() - 0.5f) / (max / size));
+            }
+        }
+        FractalCreation(half);
+    }
+
+    //diamonds are created on the terrain to make the terrain itself seem rougher example a rocky terrain
+    void Diamond(int x, int y, int size, float offset)
+    {
+        float ave = Average(new float[4]{
+            Get(x, y - size),
+            Get(x + size, y),
+            Get(x, y + size),
+            Get(x - size, y)});
+
+        heightmap[x, y] = ave + offset * roughness;
+    }
+
+    //Squares are created on the terrain to make the terrain itself seem rougher example a rocky terrain
+    void Square(int x, int y, int size, float offset)
+    {
+        float ave = Average(new float[4]{
+        Get (x - size, y - size),
+        Get (x + size, y - size),
+        Get (x + size, y + size),
+        Get (x - size, y + size)});
+
+        heightmap[x, y] = ave + offset * roughness;
+    }
+
+    //making sure the x and y vlaues of the heightmap are above 0 but not bigger than max value
+    float Get(int x, int y)
+    {
+        if (x < 0 || x > max || y < 0 || y > max)
+        {
+            return 0;
+        }
+        return heightmap[x, y];
+    }
+
+    //finds the average of the float values and divides them by total
+    float Average(float[] values)
+    {
+        float total = 0;
+        foreach (float f in values)
+        {
+            total += f;
+        }
+        return total / values.Length;
+    }
+
+
+}
---
+++ Perlin noise/Map.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshFilter))]
+public class Map : MonoBehaviour {
+    Mesh mesh;
+     
+    //vector3 called vertices used to create a mesh
+    Vector3[] vertices;
+    int[] triangles;
+
+    //x and y values of the plane
+    public int xSize = 20;
+    public int zSize = 20;
+
+	
+	void Start ()
+    {
+        //creates a mesh
+        mesh = new Mesh();
+        //gets the mesh component and assigns it to mesh
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        //starts the coroutine to create the triangles
+        StartCoroutine(CreateShape());
+        
+	}
+
+    private void Update()
+    {
+        //updates the mesh every frame as each vertice is populated with a value 
+        UpdateMesh();
+    }
+
+    
+    IEnumerator CreateShape()
+    {
+        //populates the vertices with the xSize value and the zSize values 
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        
+        for(int i = 0, z = 0; z <= zSize; z++)
+        {
+            for(int x = 0; x <= xSize; x++)
+            {
+                //manipulates the y float with the perlin noise equation timesing both the x and z values by random integers between 0 and 1
+                float y = Mathf.PerlinNoise(x * Random.Range(0.0f, 1.0f), z * Random.Range(0.0f, 1.0f)) * 3f;
+                vertices[i] = new Vector3(x, y, z);
+                i++;
+            }
+        }
+
+        //fills tirangles with a new integer created by timesing xSize by zSize and the number 6
+        //number 6 being the number of triangles created below 
+        triangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+        
+        //for loop to repeat the creation of the triangles until the grid is filled
+        for(int z = 0; z < zSize; z++)
+        {
+            //second for loop to create the original siz triangles 
+            for (int x = 0; x < xSize; x++)
+            {
+
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+
+                yield return new WaitForSeconds(.01f);
+            }
+            vert++;
+        }
+        
+
+
+     
+
+
+    }
+
+    //makes sure the mesh is clear to begin with before filling it with the vertices and triangles.
+    void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+    }
+
+    //draws a sphere used for testing the y value of the map
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+            return;
+
+        for(int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], .1f);
+        }
+    }
+}
